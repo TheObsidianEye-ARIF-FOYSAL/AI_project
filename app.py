a959ff4da69ff5f97499eeae25160e50d9857352
Cyb3r-Bishop <ariffoysalruetcse21@gmail.com>
2025-12-31 06:43:06 +0600

    hih

--- app.py
+++ app.py
@@ -1,34 +1,39 @@
-from flask import Flask, request, jsonify
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from flask import Flask, request, jsonify, render_template
+import tensorflow as tf
 import numpy as np
+from tensorflow.keras.preprocessing import image
 import os
 
 app = Flask(__name__)
 
-model = load_model("animals10_model.h5")
-class_names = [
-    'butterfly', 'cat', 'chicken', 'cow',
-    'dog', 'elephant', 'horse', 'sheep',
-    'spider', 'squirrel'
-]
+# Load model once
+model = tf.keras.models.load_model("animals10_model.h5")
 
+# Home page
+@app.route("/")
+def home():
+    return render_template("index.html")  # or simple text
+
+# Prediction API
 @app.route("/predict", methods=["POST"])
 def predict():
-    file = request.files['image']
-    img_path = "temp.png"
-    file.save(img_path)
+    if 'file' not in request.files:
+        return jsonify({"error": "No file uploaded"}), 400
 
-    img = image.load_img(img_path, target_size=(224,224))
-    img = image.img_to_array(img) / 255.0
-    img = np.expand_dims(img, axis=0)
+    img_file = request.files['file']
+    img_path = os.path.join("uploads", img_file.filename)
+    img_file.save(img_path)
+
+    # Preprocess image
+    IMG_SIZE = 224
+    img = image.load_img(img_path, target_size=(IMG_SIZE, IMG_SIZE))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0) / 255.0
 
     pred = model.predict(img)
-    label = class_names[np.argmax(pred)]
-
-    os.remove(img_path)
-
-    return jsonify({"prediction": label})
+    class_names = list(model.class_names) if hasattr(model, "class_names") else ["cane","cavallo","elefante","farfalla","gallina","gatto","mucca","pecora","ragno","scoiattolo"]
+    result = class_names[np.argmax(pred)]
+    return jsonify({"prediction": result})
 
 if __name__ == "__main__":
     app.run(debug=True)
