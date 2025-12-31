495ea007b1930671e9bf73d3d145b2bf796a90c4
Cyb3r-Bishop <ariffoysalruetcse21@gmail.com>
2025-12-31 06:43:38 +0600

    hih

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def predict():
     return jsonify({"prediction": result})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
